ac9a8452472ade3c701eb1a616dba7fbd6253f07
Fatih Çakmak <cakmakfatih@outlook.com.tr>
2019-01-10 01:34:17 +0300

    map rotated

--- scripts/app.js
+++ scripts/app.js
@@ -77375,39 +77375,37 @@ class Builder {
         this.update = this.update.bind(this);
         this.viewLoop = this.viewLoop.bind(this);
         this.scene = new three__WEBPACK_IMPORTED_MODULE_0__["Scene"]();
-        this.camera = new three__WEBPACK_IMPORTED_MODULE_0__["PerspectiveCamera"](75, window.innerWidth / window.innerHeight, 0.1, 100000);
-        this.renderer = new three__WEBPACK_IMPORTED_MODULE_0__["WebGLRenderer"]();
+        this.camera = new three__WEBPACK_IMPORTED_MODULE_0__["PerspectiveCamera"](75, container.getBoundingClientRect().width / container.getBoundingClientRect().height, 0.1, 100000);
+        this.renderer = new three__WEBPACK_IMPORTED_MODULE_0__["WebGLRenderer"]({ antialias: true });
         this.renderer.shadowMap.enabled = true;
-        this.renderer.setSize(container.getBoundingClientRect().width, window.innerHeight);
+        this.renderer.setSize(container.getBoundingClientRect().width, container.getBoundingClientRect().height);
         container.appendChild(this.renderer.domElement);
         window.addEventListener('resize', () => {
-            this.renderer.setSize(container.getBoundingClientRect().width, window.innerHeight);
-            this.camera.aspect = container.offsetWidth / window.innerHeight;
+            this.renderer.setSize(container.getBoundingClientRect().width, container.getBoundingClientRect().height);
+            this.camera.aspect = container.offsetWidth / container.getBoundingClientRect().height;
             this.camera.updateProjectionMatrix();
         });
-        this.camera.position.z = 50;
-        this.camera.position.y = 50;
-        this.camera.position.x = 100;
-        this.camera.lookAt(30, 40, 60);
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-        //this.addSkybox();
-        this.addMap();
+        this.addSkybox();
         this.viewLoop();
-    }
-    addMap() {
-        var geometry = new three__WEBPACK_IMPORTED_MODULE_0__["PlaneGeometry"](5000, 5000, 2500, 2500);
-        let textureLoader = new three__WEBPACK_IMPORTED_MODULE_0__["TextureLoader"]();
-        // https://api.mapbox.com/styles/v1/mapbox/light-v9/static/29.07107,41.00360,15.0,0,0/1280x1280?access_token=pk.eyJ1IjoiY2FrbWFrZmF0aWgiLCJhIjoiY2pxcGk1d3ZrMDFwYjQ5bzFqNncyYjl2NyJ9.MtGJZ74Cu-6R7K52rFrNeQ
-        let map = textureLoader.load("staticmap.png");
-        map.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
-        map.minFilter = three__WEBPACK_IMPORTED_MODULE_0__["LinearFilter"];
-        var material = new three__WEBPACK_IMPORTED_MODULE_0__["MeshBasicMaterial"]({ map: map, side: three__WEBPACK_IMPORTED_MODULE_0__["DoubleSide"] });
-        var plane = new three__WEBPACK_IMPORTED_MODULE_0__["Mesh"](geometry, material);
-        plane.rotation.x += Math.PI / 2;
-        this.scene.add(plane);
     }
     addSkybox() {
         // to-do add skybox
+        let geometry = new three__WEBPACK_IMPORTED_MODULE_0__["BoxGeometry"](10000, 10000, 10000);
+        let textureLoader = new three__WEBPACK_IMPORTED_MODULE_0__["TextureLoader"]();
+        let mapUrl = "https://api.mapbox.com/styles/v1/mapbox/light-v9/static/29.07107,41.00360,15.0,0,0/1280x1280?access_token=pk.eyJ1IjoiY2FrbWFrZmF0aWgiLCJhIjoiY2pxcGk1d3ZrMDFwYjQ5bzFqNncyYjl2NyJ9.MtGJZ74Cu-6R7K52rFrNeQ";
+        let map = textureLoader.load("staticmap.png");
+        map.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
+        map.minFilter = three__WEBPACK_IMPORTED_MODULE_0__["NearestFilter"];
+        map.wrapS = three__WEBPACK_IMPORTED_MODULE_0__["RepeatWrapping"];
+        map.repeat.x = -1;
+        let mat = new three__WEBPACK_IMPORTED_MODULE_0__["MeshBasicMaterial"]({
+            side: three__WEBPACK_IMPORTED_MODULE_0__["DoubleSide"],
+            map: map
+        });
+        let cube = new three__WEBPACK_IMPORTED_MODULE_0__["Mesh"](geometry, mat);
+        cube.position.setY(5000);
+        this.scene.add(cube);
     }
     addLights() {
         // to-do add lights
@@ -77425,6 +77423,8 @@ class Builder {
     }
     setOffsets(coords) {
         this.vectorGenerator = new _models_scaledvector_model__WEBPACK_IMPORTED_MODULE_1__["VectorGenerator"](undefined, coords[0], coords[1]);
+        this.camera.position.set(700, 700, -700);
+        this.camera.lookAt(0, 0, 0);
     }
     setVenueColor(color) {
         this.venue.material.color.setHex(color);
@@ -77455,7 +77455,7 @@ class Builder {
         });
         let geometry = new three__WEBPACK_IMPORTED_MODULE_0__["ExtrudeBufferGeometry"](shape, extrudeSettings);
         let mesh = new three__WEBPACK_IMPORTED_MODULE_0__["Mesh"](geometry, [material, sidesMaterial]);
-        mesh.rotation.x -= Math.PI / 2;
+        mesh.rotation.x += -Math.PI / 2;
         this.scene.add(mesh);
     }
     addMultiPolygon(i) {
@@ -77467,7 +77467,7 @@ class Builder {
             j.forEach((k) => {
                 k.forEach((q) => {
                     let scaledVector = this.vectorGenerator.generateVector(q);
-                    let vector = new three__WEBPACK_IMPORTED_MODULE_0__["Vector3"](scaledVector.x, scaledVector.y, scaledVector.z);
+                    let vector = new three__WEBPACK_IMPORTED_MODULE_0__["Vector3"](-scaledVector.x, scaledVector.y, scaledVector.z);
                     geometry.vertices.push(vector);
                 });
             });
