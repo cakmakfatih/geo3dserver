a1fd7cc5f8a706575f0f4aef25f79b6000400c54
Fatih Çakmak <cakmakfatih@outlook.com.tr>
2019-01-09 23:28:22 +0300

    import update

--- scripts/app.js
+++ scripts/app.js
@@ -77370,13 +77370,13 @@ const OrbitControls = __webpack_require__(/*! three-orbit-controls */ "./node_mo
 class Builder {
     constructor(data, container) {
         this.data = data;
-        this.venueColor = 0x000000;
+        this.venueColor = 0x00ff00;
         this.render = this.render.bind(this);
         this.update = this.update.bind(this);
         this.viewLoop = this.viewLoop.bind(this);
         this.scene = new three__WEBPACK_IMPORTED_MODULE_0__["Scene"]();
         this.camera = new three__WEBPACK_IMPORTED_MODULE_0__["PerspectiveCamera"](75, window.innerWidth / window.innerHeight, 0.1, 100000);
-        this.renderer = new three__WEBPACK_IMPORTED_MODULE_0__["WebGLRenderer"]({ antialias: true });
+        this.renderer = new three__WEBPACK_IMPORTED_MODULE_0__["WebGLRenderer"]();
         this.renderer.shadowMap.enabled = true;
         this.renderer.setSize(container.getBoundingClientRect().width, window.innerHeight);
         container.appendChild(this.renderer.domElement);
@@ -77390,23 +77390,24 @@ class Builder {
         this.camera.position.x = 100;
         this.camera.lookAt(30, 40, 60);
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-        this.addSkybox();
+        //this.addSkybox();
+        this.addMap();
         this.viewLoop();
     }
-    addSkybox() {
-        // to-do add skybox
-        let geometry = new three__WEBPACK_IMPORTED_MODULE_0__["BoxGeometry"](5000, 5000, 5000);
+    addMap() {
+        var geometry = new three__WEBPACK_IMPORTED_MODULE_0__["PlaneGeometry"](5000, 5000, 2500, 2500);
         let textureLoader = new three__WEBPACK_IMPORTED_MODULE_0__["TextureLoader"]();
         // https://api.mapbox.com/styles/v1/mapbox/light-v9/static/29.07107,41.00360,15.0,0,0/1280x1280?access_token=pk.eyJ1IjoiY2FrbWFrZmF0aWgiLCJhIjoiY2pxcGk1d3ZrMDFwYjQ5bzFqNncyYjl2NyJ9.MtGJZ74Cu-6R7K52rFrNeQ
         let map = textureLoader.load("staticmap.png");
+        map.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
         map.minFilter = three__WEBPACK_IMPORTED_MODULE_0__["LinearFilter"];
-        let mat = new three__WEBPACK_IMPORTED_MODULE_0__["MeshBasicMaterial"]({
-            side: three__WEBPACK_IMPORTED_MODULE_0__["DoubleSide"],
-            map: map
-        });
-        let cube = new three__WEBPACK_IMPORTED_MODULE_0__["Mesh"](geometry, mat);
-        cube.position.setY(2500);
-        this.scene.add(cube);
+        var material = new three__WEBPACK_IMPORTED_MODULE_0__["MeshBasicMaterial"]({ map: map, side: three__WEBPACK_IMPORTED_MODULE_0__["DoubleSide"] });
+        var plane = new three__WEBPACK_IMPORTED_MODULE_0__["Mesh"](geometry, material);
+        plane.rotation.x += Math.PI / 2;
+        this.scene.add(plane);
+    }
+    addSkybox() {
+        // to-do add skybox
     }
     addLights() {
         // to-do add lights
@@ -77454,7 +77455,7 @@ class Builder {
         });
         let geometry = new three__WEBPACK_IMPORTED_MODULE_0__["ExtrudeBufferGeometry"](shape, extrudeSettings);
         let mesh = new three__WEBPACK_IMPORTED_MODULE_0__["Mesh"](geometry, [material, sidesMaterial]);
-        mesh.rotation.x += -Math.PI / 2;
+        mesh.rotation.x -= Math.PI / 2;
         this.scene.add(mesh);
     }
     addMultiPolygon(i) {
